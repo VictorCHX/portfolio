fa3c507ea46141f9289df9b04f87d6c6d097db65
victor <victor.lafargechx@gmail.com>
2021-11-20 12:02:31 +0100

    debut ajout mouvement avec balles

--- ressources/js/gridBg.js
+++ ressources/js/gridBg.js
@@ -1,4 +1,34 @@
 const tileSize = 40
+
+class Circle {
+    tileSize = 40
+    pos = {
+        x:0,
+        y:0
+    } 
+    size = 10
+    speed = 10
+    constructor(pos) {
+        this.pos = pos
+        this.setRandomDirection()
+    }
+
+    move() {
+        this.pos.x += this.speed;
+        this.pos.y += this.speed;
+        if(this.pos.x % tileSize == 1 || this.pos.y % tileSize == 1)
+        {
+            this.setRandomDirection()
+            //console.log(this);
+        }
+    }
+
+    setRandomDirection()
+    {
+        this.speed = Math.floor(-1 + Math.random() * 3)*30
+    }
+}
+
 document.addEventListener('DOMContentLoaded', ()=> {
 
 
@@ -8,44 +38,65 @@ document.addEventListener('DOMContentLoaded', ()=> {
     // ...then set the internal size to match
     canvas.width  = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
+    let circles = [new Circle({x:0,y:0})]
     
     let ctx = canvas.getContext('2d');
-    ctx.lineWidth = 1;
         
     displayGrid(canvas, ctx)
-    after(canvas, ctx)
+    //after(canvas, ctx)
 
-        
-})
-
-function displayGrid(canvas, ctx)
-{
-    for(var x=-1;x<canvas.width;x+=tileSize) {
-        ctx.moveTo(x,0);
-        ctx.lineTo(x,canvas.height);
-    }
-
-    for(var y=-1; y<canvas.height; y+=tileSize) {
-        ctx.moveTo(0,y);
-        ctx.lineTo(canvas.width,y);
+    function drawBalls() {
+        for(let circle of circles) {
+            ctx.beginPath();
+            ctx.arc(circle.pos.x, circle.pos.y, circle.size, 0, Math.PI*2);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+            circle.move()
+        }
     }
     
-    ctx.strokeStyle='white';
-    ctx.stroke();
-}
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        displayGrid()
+        drawBalls();
+        
+    }
 
-function after(canvas, ctx)
-{
+    function displayGrid()
+    {
+        ctx.beginPath();
+        for(var x=-1;x<canvas.width;x+=tileSize) {
+            ctx.moveTo(x,0);
+            ctx.lineTo(x,canvas.height);
+        }
+
+        for(var y=-1; y<canvas.height; y+=tileSize) {
+            ctx.moveTo(0,y);
+            ctx.lineTo(canvas.width,y);
+        }
+        
+        ctx.strokeStyle='white';
+        ctx.stroke();
+        ctx.closePath();
+    }
+
     canvas.addEventListener("mousemove", (e)=>{
         let pos = {
-            x:e.offsetX,
-            y:e.offsetY
-        } 
-        console.log("Left? : " + pos.x + " ; Top? : " + pos.y + ".");
-        ctx.moveTo(pos.x, pos.y)
-        ctx.lineTo(500,pos.y);
-        ctx.fillStyle ='pink';
-        ctx.stroke();
+            x: Math.floor(e.offsetX),
+            y: Math.floor(e.offsetY)
+        }
+        if(pos.x % tileSize == 1 || pos.y % tileSize == 1)
+        {
+            circles.push(new Circle(pos))
+        }
 
     })
+    
+    setInterval(draw, 100);
+})
+
+function getRandomInt(min, max) {
+    return Math.floor(min + Math.random() * (max - min +1));
 }
+
