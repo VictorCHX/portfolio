bfb105420c3affa78e96179f2926a72913c27ddd
victor <victor.lafargechx@gmail.com>
2021-11-23 16:30:15 +0100

    delete cursor on mobile

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    let cursor = document.querySelector("#cursor");
-    let cursorWidth = cursor.clientWidth
-    window.addEventListener("mousemove", e => {
-        let posX = e.pageX - cursor.clientWidth
-        let posY = e.pageY - cursor.clientHeight
-        cursor.style.left = posX + "px";
-        cursor.style.top = posY+ "px";
-    });
+    if(window.innerWidth > 765)
+    {
+
+        let cursor = document.querySelector("#cursor");
+        let cursorWidth = cursor.clientWidth
+        window.addEventListener("mousemove", e => {
+            let posX = e.pageX - cursor.clientWidth
+            let posY = e.pageY - cursor.clientHeight
+            cursor.style.left = posX + "px";
+            cursor.style.top = posY+ "px";
+        });
+    }
 })
